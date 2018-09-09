10ea153cac646ca0f723805f6c425b5a15500ba2
James Connolly <james.connolly@lyit.ie>
2018-09-09 16:42:05 +0100

    else-if-code

--- DecisionsAndLoops/DecisionsAndLoops/Program.cs
+++ DecisionsAndLoops/DecisionsAndLoops/Program.cs
@@ -10,6 +10,41 @@ namespace DecisionsAndLoops
     {
         static void Main(string[] args)
         {
+            int score;
+            Console.WriteLine("Enter your score");
+            string scoreAsText = Console.ReadLine();
+            score = Convert.ToInt16(scoreAsText);
+            if (score == 100)
+            {
+                // The stuff here inside the if statement
+                // only gets executed when the condition is met
+                Console.WriteLine("Perfect score! You win!");
+            }
+            if (score >= 90)
+            {
+                Console.WriteLine("You got an A.");
+            }
+            else if (score >=80)
+            {
+                Console.WriteLine("You got an B.");
+            }
+            else if (score >= 70)
+            {
+                Console.WriteLine("You got an C.");
+            }
+            else if (score >= 60)
+            {
+                Console.WriteLine("You got an D.");
+            }          
+            else
+            {
+                Console.WriteLine("You got an F.");
+            }
+           
+            // Everything down here after the if statement will
+            // also be executed.
+            Console.ReadKey();
+
         }
     }
 }
